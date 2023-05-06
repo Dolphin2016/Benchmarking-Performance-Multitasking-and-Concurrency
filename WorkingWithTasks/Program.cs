998e4aa7b11b674ed30e4977d9929f4e4013eb24
Dolphin2016 <evgenij55555@gmail.com>
2023-05-06 14:55:13 +0500

    Running Multiple Actions Asynchronously Using Tasks

--- WorkingWithTasks/Program.cs
+++ WorkingWithTasks/Program.cs
@@ -5,9 +5,19 @@ using System.Diagnostics;
 OutputThreadInfo();
 Stopwatch timer = Stopwatch.StartNew();
 
-SectionTitle("Running methods synchronously on one thread.");
+/*SectionTitle("Running methods synchronously on one thread.");
 MethodA();
 MethodB();
 MethodC();
+
+WriteLine($"{timer.ElapsedMilliseconds:#,##0}ms elapsed.");*/
+
+SectionTitle("Running methods asynchronously on multiple threads.");
+timer.Restart();
+
+Task taskA = new(MethodA);
+taskA.Start();
+Task taskB = Task.Factory.StartNew(MethodB);
+Task taskC = Task.Run(MethodC);
 
 WriteLine($"{timer.ElapsedMilliseconds:#,##0}ms elapsed.");
