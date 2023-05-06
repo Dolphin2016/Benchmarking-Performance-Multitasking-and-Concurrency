d7fd46bc538d36a07085d0d7e36127a43fa42fce
Dolphin2016 <evgenij55555@gmail.com>
2023-05-06 13:30:44 +0500

    Measuring the Efficiency of Processing Strings

---
+++ MonitoringApp/Program.Helpers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+partial class Program
+{
+    static void SectionTitle(string title)
+    {
+        ConsoleColor previuosColor = ForegroundColor;
+        ForegroundColor = ConsoleColor.DarkYellow;
+        WriteLine("*");
+        WriteLine($"* {title}");
+        WriteLine("*");
+        ForegroundColor = previuosColor;
+    }
+}
